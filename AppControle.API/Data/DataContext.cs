7f4b6035cc0a68de64d5217dcbbaa83796d3145c
brunomattosb <brunobragagm@gmail.com>
2024-01-09 17:52:53 -0300

    Iniciando Clientes

--- AppControle.API/Data/DataContext.cs
+++ AppControle.API/Data/DataContext.cs
@@ -13,6 +13,7 @@ namespace AppControle.API.Data
         public DbSet<Country>? Countries { get; set; }
         public DbSet<City>? Cities { get; set; }
         public DbSet<State>? States { get; set; }
+        public DbSet<Client> Clients { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -20,7 +21,7 @@ namespace AppControle.API.Data
             modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
             modelBuilder.Entity<State>().HasIndex("CountryId", "Name").IsUnique();
             modelBuilder.Entity<City>().HasIndex("StateId", "Name").IsUnique();
-
+            modelBuilder.Entity<Client>().HasIndex(x => x.Cpf_Cnpj).IsUnique();
             modelBuilder.Entity<User>().HasIndex("Cpf_Cnpj").IsUnique();
 
         }
--- AppControle.API/Data/SeedDb.cs
+++ AppControle.API/Data/SeedDb.cs
@@ -6,6 +6,7 @@ using AppControle.Shared.Enums;
 using AppControle.Shared.Responses;
 using Microsoft.EntityFrameworkCore;
 using SisVendas.Shared.Responses;
+using System.Runtime.InteropServices;
 
 namespace SisVendas.API.Data
 {
@@ -30,14 +31,14 @@ namespace SisVendas.API.Data
             await CheckCountriesAsync();
             //await CheckCategoriesAsync();
             await CheckRolesAsync();
-            //await CheckUserAsync("1010", "Juan", "Zuluaga", "zulu@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "JuanZuluaga.jpeg", UserType.Admin);
-            //await CheckUserAsync("2020", "Ledys", "Bedoya", "ledys@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "LedysBedoya.jpeg", UserType.User);
-            //await CheckUserAsync("3030", "Brad", "Pitt", "brad@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "Brad.jpg", UserType.User);
-            //await CheckUserAsync("4040", "Angelina", "Jolie", "angelina@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "Angelina.jpg", UserType.User);
-            //await CheckUserAsync("5050", "Bob", "Marley", "bob@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "bob.jpg", UserType.User);
+            await CheckUserAsync("1010", "Juan", "Zuluaga", "zulu@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "JuanZuluaga.jpeg", UserType.Admin);
+            await CheckUserAsync("2020", "Ledys", "Bedoya", "ledys@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "LedysBedoya.jpeg", UserType.User);
+            await CheckUserAsync("3030", "Brad", "Pitt", "brad@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "Brad.jpg", UserType.User);
+            await CheckUserAsync("4040", "Angelina", "Jolie", "angelina@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "Angelina.jpg", UserType.User);
+            await CheckUserAsync("5050", "Bob", "Marley", "bob@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "bob.jpg", UserType.User);
             //await CheckProductsAsync();
 
-            //await CheckClientsAsync();
+            await CheckClientsAsync();
         }
 
         //private async Task CheckProductsAsync()
@@ -138,80 +139,93 @@ namespace SisVendas.API.Data
         //        await _context.SaveChangesAsync();
         //    }
         //}
-        //private async Task CheckClientsAsync()
-        //{
-        //    if (!_context.Clients.Any())
-        //    {
-        //        City city = await _context.Cities!.FirstOrDefaultAsync();
-        //        User user = await _context.Users!.FirstOrDefaultAsync(c => c.Cpf_Cnpj == "1010")!;
-        //        _context.Clients.Add(new Client {
-        //            Name = "Client 1",
-        //            Cpf_Cnpj = "38713376845",
+        private async Task CheckClientsAsync()
+        {
+            if (!_context.Clients.Any())
+            {
+                City city = await _context.Cities!.FirstOrDefaultAsync();
+                User user = await _context.Users!.FirstOrDefaultAsync(c => c.Cpf_Cnpj == "1010")!;
+                _context.Clients.Add(new Client
+                {
+                    Name = "Client 1",
+                    Cpf_Cnpj = "38713376845",
 
 
-        //            BirthData = DateTime.Now,
-        //            Neighborhood = "Bairro Um",
-        //            City = city,
-        //            CityId = city.Id,
+                    BirthData = DateTime.Now,
+                    Neighborhood = "Bairro Um",
+                    City = city,
+                    CityId = city.Id,
 
-        //            User = user,
-        //            UserId = user.Id
-        //        });
-        //        await _context.SaveChangesAsync();
-        //    }
-        //}
+                    User = user,
+                    UserId = user.Id
+                }); _context.Clients.Add(new Client
+                {
+                    Name = "Client 2",
+                    Cpf_Cnpj = "06960118832",
+
+
+                    BirthData = DateTime.Now,
+                    Neighborhood = "Bairro dois",
+                    City = city,
+                    CityId = city.Id,
+
+                    User = user,
+                    UserId = user.Id
+                });
+                await _context.SaveChangesAsync();
+            }
+        }
         private async Task CheckRolesAsync()
         {
             await _userHelper.CheckRoleAsync(UserType.Admin.ToString());
             await _userHelper.CheckRoleAsync(UserType.User.ToString());
         }
 
-        //private async Task<User> CheckUserAsync(string document, string firstName, string lastName, string email, string phone, string address, string image, UserType userType)
-        //{
-        //    var user = await _userHelper.GetUserAsync(email);
-        //    if (user == null)
-        //    {
-        //        var city = await _context.Cities.FirstOrDefaultAsync(x => x.Name == "Medellín");
-        //        if (city == null)
-        //        {
-        //            city = await _context.Cities.FirstOrDefaultAsync();
-        //        }
+        private async Task<User> CheckUserAsync(string document, string firstName, string lastName, string email, string phone, string address, string image, UserType userType)
+        {
+            var user = await _userHelper.GetUserAsync(email);
+            if (user == null)
+            {
+                var city = await _context.Cities!.FirstOrDefaultAsync(x => x.Name == "Medellín");
+                if (city == null)
+                {
+                    city = await _context.Cities!.FirstOrDefaultAsync();
+                }
 
-        //        string filePath;
-        //        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        //        {
-        //            filePath = $"{Environment.CurrentDirectory}\\Images\\users\\{image}";
-        //        }
-        //        else
-        //        {
-        //            filePath = $"{Environment.CurrentDirectory}/Images/users/{image}";
-        //        }
+                //string filePath;
+                //if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                //{
+                //    filePath = $"{Environment.CurrentDirectory}\\Images\\users\\{image}";
+                //}
+                //else
+                //{
+                //    filePath = $"{Environment.CurrentDirectory}/Images/users/{image}";
+                //}
 
-        //        var fileBytes = File.ReadAllBytes(filePath);
-        //        var imagePath = await _fileStorage.SaveFileAsync(fileBytes, "jpg", "users");
+                //var fileBytes = File.ReadAllBytes(filePath);
+                //var imagePath = await _fileStorage.SaveFileAsync(fileBytes, "jpg", "users");
 
-        //        user = new User
-        //        {
-        //            Name = firstName + " " + lastName,
-        //            Cpf_Cnpj = document,
-        //            Email = email,
-        //            UserName = email,
-        //            PhoneNumber = phone,
-        //            Address = address,
-        //            City = city,
-        //            UserType = userType,
-        //            Photo = imagePath,
-        //        };
+                user = new User
+                {
+                    Name = firstName + " " + lastName,
+                    Cpf_Cnpj = document,
+                    Email = email,
+                    UserName = email,
+                    PhoneNumber = phone,
+                    Address = address,
+                    City = city,
+                    UserType = userType,
+                };
 
-        //        await _userHelper.AddUserAsync(user, "123456");
-        //        await _userHelper.AddUserToRoleAsync(user, userType.ToString());
+                await _userHelper.AddUserAsync(user, "123456");
+                await _userHelper.AddUserToRoleAsync(user, userType.ToString());
 
-        //        var token = await _userHelper.GenerateEmailConfirmationTokenAsync(user);
-        //        await _userHelper.ConfirmEmailAsync(user, token);
-        //    }
+                var token = await _userHelper.GenerateEmailConfirmationTokenAsync(user);
+                await _userHelper.ConfirmEmailAsync(user, token);
+            }
 
-        //    return user;
-        //}
+            return user;
+        }
 
 
         private async Task CheckCountriesAsync()
