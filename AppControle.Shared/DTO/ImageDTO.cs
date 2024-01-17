8152b3a36493b426e73a316e4f16a48cc43c0a7f
bruno <brunobragagm@gmail.com>
2024-01-17 13:07:51 -0300

    Alterando imageselector

---
+++ AppControle.Shared/DTO/ImageDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControle.Shared.DTO
+{
+    public class ImageDTO
+    {
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public List<string> Images { get; set; } = null!;
+    }
+}
