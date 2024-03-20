56a841f07dfe3418c5d7ed6de04614408287b273
brunomattosb <brunobragagm@gmail.com>
2024-03-20 17:30:49 -0300

    Tentando resolver esse bug dos shared

---
+++ AppControle.DeskMob/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppControle.DeskMob
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ AppControle.DeskMob/MauiProgram.cs
@@ -0,0 +1,43 @@
+﻿using AppControle.Hybrid.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.Logging;
+
+namespace AppControle.DeskMob
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
+#endif
+
+            builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7175/") }); //7175 //44380
+            //builder.Services.AddScoped<IRepository, Repository>();
+            //Modal
+            //builder.Services.AddBlazoredModal();
+            //Auth
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationProviderJWT>();
+            builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
+            //builder.Services.AddScoped<ILoginService, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
+            //builder.Services.AddSweetAlert2();
+            //builder.Services.AddBlazoredModal();
+
+
+            return builder.Build();
+        }
+    }
+}
